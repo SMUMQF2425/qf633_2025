fef28f4616ef7b75e614c8c96d0c0492c750e36e
mqf class of '25 <ric.chan.2024@mqf.smu.edu.sg>
2025-05-12 21:56:55 +0800

    Add files via upload

---
+++ code_L5/assignment/AmericanTrade.h
@@ -0,0 +1,78 @@
+#ifndef _AMERICAN_TRADE
+#define _AMERICAN_TRADE
+
+#include <cassert>
+
+#include "TreeProduct.h"
+#include "Types.h"
+#include "Payoff.h"
+
+class AmericanOption : public TreeProduct
+{
+public:
+  // AmericanOption(const string& name, OptionType _optType, double _strike, const Date& _expiry):
+  // asset(name), optType(_optType), strike(_strike), expiryDate(_expiry) {}
+  AmericanOption(const std::string &name, OptionType _optType, double _strike, const Date &_expiry)
+      : TreeProduct(name), optType(_optType), strike(_strike), expiryDate(_expiry) {}
+
+  virtual double Payoff(double S) const
+  {
+    return PAYOFF::VanillaOption(optType, strike, S);
+  }
+  virtual const Date &GetExpiry() const
+  {
+    return expiryDate;
+  }
+  virtual double ValueAtNode(double S, double t, double continuation) const
+  {
+    return std::max(Payoff(S), continuation);
+  }
+
+  // Inline public member function to return optType
+  OptionType getOptionType() const { return optType; }
+  // Getter for strike price
+  double getStrike() const { return strike; }
+
+  // Getter for expiry date
+  const Date &getExpiryDate() const { return expiryDate; }
+
+private:
+  string asset;
+  OptionType optType;
+  double strike;
+  Date expiryDate;
+};
+
+class AmerCallSpread : public AmericanOption
+{
+public:
+  AmerCallSpread(const std::string &name, double _k1, double _k2, const Date &_expiry)
+      : AmericanOption(name, Call, _k1, _expiry), strike1(_k1), strike2(_k2)
+  {
+    assert(_k1 < _k2);
+  }
+
+  virtual double Payoff(double S) const
+  {
+    return PAYOFF::CallSpread(strike1, strike2, S);
+  }
+  virtual const Date &GetExpiry() const
+  {
+    return expiryDate;
+  }
+  //// Getter for expiry date
+  // const Date& getExpiryDate() const { return expiryDate; }
+  //  Getter for the first strike price
+  double getStrike1() const { return strike1; }
+
+  // Getter for the second strike price
+  double getStrike2() const { return strike2; }
+
+private:
+  string asset;
+  double strike1;
+  double strike2;
+  Date expiryDate;
+};
+
+#endif
---
+++ code_L5/assignment/Swap.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "Trade.h"
+
+class Swap : public Trade
+{
+public:
+    // make necessary change
+    //  Constructor
+    Swap(const std::string &curveName, Date start, Date end, double _notional, double _tradeRate, int _frequency)
+        : Trade("SwapTrade", start), curveName(curveName), startDate(start), maturityDate(end), notional(_notional), tradeRate(_tradeRate)
+    {
+        // Validate frequency
+        valid = true;
+        if (_frequency != 1 && _frequency != 2 && _frequency != 3 && _frequency != 4 && _frequency != 6 && _frequency != 12)
+        {
+            cout << "Frequency must be one of the values: [1, 2, 3, 4, 6, 12]" << endl;
+            valid = false;
+        }
+        frequency = _frequency;
+    }
+
+    /*
+    implement this, using pv = annuity * (trade rate - market rate);
+    approximately, annuity = sum of (notional * Discount factor at each period end);
+    for a 2 year swap, with annual freq and notinal 100, annuity = 100 * Df(at year 1 end) + 100* Df(at year 2 end);
+    Df = exp(-rT), r taken from curve;
+    */
+    // inline double Payoff(double marketRate) const { return 0; };
+
+    // Override the Payoff method with one parameter
+    double Payoff(double marketRate) const override;
+
+    // Additional Payoff method with two parameters
+    double Payoff(double marketRate, double annuity) const;
+
+    // Implement getAnnuity method
+    double getMktRate(const RateCurve &rates) const;
+    double getAnnuity(const RateCurve &rates) const;
+    // double getAnnuity() const; //implement this in a cpp file
+    //  Getter methods for each member variable
+    string getCurveName() const { return curveName; }
+    Date getStartDate() const { return startDate; }
+    Date getMaturityDate() const { return maturityDate; }
+    double getNotional() const { return notional; }
+    double getTradeRate() const { return tradeRate; }
+    int getFrequency() const { return frequency; }
+    bool getValid() const { return valid; }
+
+private:
+    std::string curveName;
+    Date startDate;
+    Date maturityDate;
+    double notional;
+    double tradeRate;
+    int frequency; // use 1 for annual, 2 for semi-annual etc
+    bool valid;
+};
